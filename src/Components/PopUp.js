4236c5ddf572de0a5039c9d674f899e65d05b6d8
Florid11 <florid.arifaj@hotmail.com>
2022-02-20 23:06:37 +0100

    Components created all

--- src/Components/PopUp.js
+++ src/Components/PopUp.js
@@ -1,31 +1,39 @@
 import { ColorPicker } from 'material-ui-color';
+import {useState} from "react";
 
 const PopUp = (props) => {
+    const [themePop, setThemePop] =useState(false);
+    const [color, setColor] = useState('#000000');
+
     const cancelBtn = () => {
         props.onCancel();
     }
 
+    const chooseTheme = () => {
+      setThemePop(true);
+    }
+
     const saveBtn = () => {
-      props.onConfirm();
+        props.onConfirm();
     }
 
   return(
       <div className='popup'>
-          <div className='presp'>
+          <div className='buttons'>
+              <button className='themeBtn'>Change Theme</button>
+              <p><ColorPicker onChangeComplete={(color) => {setColor(color.hex)}} defaultValue={color}/></p>
+              <p><ColorPicker onChange={(color) =>{setColor(color.hex)}} defaultValue='transparent'/></p>
+              <p><ColorPicker onChange={(color) =>{setColor(color.hex)}} defaultValue='transparent'/></p>
+              <p><ColorPicker onChange={(color) =>{setColor(color.hex)}} defaultValue='transparent'/></p>
+              <p><ColorPicker onChange={(color) =>{setColor(color.hex)}} defaultValue='transparent'/></p>
+          </div>
+          <div className='presp'  style={{color: color}}>
               <h3>Theme Color</h3>
               <h4>Font Color</h4>
               <h4>Background Color</h4>
               <h4>Button Color</h4>
               <h4>Button Border Color</h4>
               <h4>Buttons Mouseover Color</h4>
-          </div>
-          <div className='buttons'>
-              <button className='themeBtn'>Change Theme</button>
-              <p><ColorPicker onChange='/' defaultValue='#000000'/></p>
-              <p><ColorPicker onChange='/' defaultValue='transparent'/></p>
-              <p><ColorPicker onChange='/' defaultValue='transparent'/></p>
-              <p><ColorPicker onChange='/' defaultValue='transparent'/></p>
-              <p><ColorPicker onChange='/' defaultValue='transparent'/></p>
           </div>
           <div className='btnBox'>
               <button onClick={cancelBtn} className='btn-p btnCancel'>Cancel</button>
