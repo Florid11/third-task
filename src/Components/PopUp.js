bd009680982b8bec323d93bb2e2e9e2053b810d9
Florid11 <florid.arifaj@hotmail.com>
2022-02-20 11:03:09 +0100

    Changes made in Css

--- src/Components/PopUp.js
+++ src/Components/PopUp.js
@@ -1,3 +1,5 @@
+import { ColorPicker } from 'material-ui-color';
+
 const PopUp = (props) => {
     const cancelBtn = () => {
         props.onCancel();
@@ -9,9 +11,27 @@ const PopUp = (props) => {
 
   return(
       <div className='popup'>
-          <h1>Hello</h1>
-          <button onClick={cancelBtn} className='btnCancel'>Cancel</button>
-          <button onClick={saveBtn} className='btnSave'>Save</button>
+          <div className='presp'>
+              <h3>Theme Color</h3>
+              <h4>Font Color</h4>
+              <h4>Background Color</h4>
+              <h4>Button Color</h4>
+              <h4>Button Border Color</h4>
+              <h4>Buttons Mouseover Color</h4>
+          </div>
+          <div className='buttons'>
+              <button className='themeBtn'>Change Theme</button>
+              <p><ColorPicker onChange='/' defaultValue='#000000'/></p>
+              <p><ColorPicker onChange='/' defaultValue='transparent'/></p>
+              <p><ColorPicker onChange='/' defaultValue='transparent'/></p>
+              <p><ColorPicker onChange='/' defaultValue='transparent'/></p>
+              <p><ColorPicker onChange='/' defaultValue='transparent'/></p>
+          </div>
+          <div className='btnBox'>
+              <button onClick={cancelBtn} className='btn-p btnCancel'>Cancel</button>
+              <button onClick={saveBtn} className='btn-p btnSave'>Save</button>
+          </div>
+
       </div>
   )
 }
