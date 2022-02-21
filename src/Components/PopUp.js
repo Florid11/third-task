724b2daaf592850474db4dfe27d69d42ed5d8b1c
Florid11 <florid.arifaj@hotmail.com>
2022-02-21 14:13:20 +0100

    Media query added

--- src/Components/PopUp.js
+++ src/Components/PopUp.js
@@ -1,4 +1,4 @@
-import { SketchPicker } from 'react-color';
+import { ChromePicker } from 'react-color';
 import {useState} from "react";
 
 const PopUp = (props) => {
@@ -24,14 +24,6 @@ const PopUp = (props) => {
 
   return(
       <div className='popup' style={{backgroundColor: color2}}>
-          <div className='buttons'>
-              <button className='themeBtn'>Change Theme</button>
-              <p>{color1}<button onClick={() => setShowColor1(showColor1 => !showColor1)} className='pick' style={{backgroundColor: color1}}/> <span>{showColor1 && (<SketchPicker color1={color1} onChangeComplete={(color1) => {setColor1(color1.hex)}}/>)}</span></p>
-              <p>{color2}<button onClick={() => setShowColor2(showColor2 => !showColor2)} className='pick' style={{backgroundColor: color2}}/><span>{showColor2 && (<SketchPicker color2={color2} onChangeComplete={(color2) => {setColor2(color2.hex)}}/>)}</span></p>
-              <p>{color3}<button onClick={() => setShowColor3(showColor3 => !showColor3)} className='pick' style={{backgroundColor: color3}}/><span>{showColor3 && (<SketchPicker color3={color3} onChangeComplete={(color3) => {setColor3(color3.hex)}}/>)}</span></p>
-              <p>{color4}<button onClick={() => setShowColor4(showColor4 => !showColor4)} className='pick' style={{backgroundColor: color4}}/><span>{showColor4 && (<SketchPicker color4={color4} onChangeComplete={(color4) => {setColor4(color4.hex)}}/>)}</span></p>
-              <p>{color5}<button onClick={() => setShowColor5(showColor5 => !showColor5)} className='pick'  style={{backgroundColor: color5}}/><span>{showColor5 && (<SketchPicker color5={color5} onChangeComplete={(color5) => {setColor5(color5.hex)}}/>)}</span></p>
-          </div>
           <div className='presp'  style={{color: color1}}>
               <h3>Theme Color</h3>
               <h4>Font Color</h4>
@@ -39,6 +31,15 @@ const PopUp = (props) => {
               <h4>Button Color</h4>
               <h4>Button Border Color</h4>
               <h4>Buttons Mouseover Color</h4>
+          </div>
+          <div className='buttons'>
+              <button className='themeBtn'>Change Theme</button>
+              <p>{color1}<button onClick={() => setShowColor1(showColor1 => !showColor1)} className='pick' style={{backgroundColor: color1}}/> <span>{showColor1 && (<ChromePicker color={color1} onChange={(color1) => {setColor1(color1.hex)}}/>)}</span></p>
+              <p>{color2}<button onClick={() => setShowColor2(showColor2 => !showColor2)} className='pick' style={{backgroundColor: color2}}/><span>{showColor2 && (<ChromePicker color={color2} onChange={(color2) => {setColor2(color2.hex)}}/>)}</span></p>
+              <p>{color3}<button onClick={() => setShowColor3(showColor3 => !showColor3)} className='pick' style={{backgroundColor: color3}}/><span>{showColor3 && (<ChromePicker color={color3} onChange={(color3) => {setColor3(color3.hex)}}/>)}</span></p>
+              <p>{color4}<button onClick={() => setShowColor4(showColor4 => !showColor4)} className='pick' style={{backgroundColor: color4}}/><span>{showColor4 && (<ChromePicker color={color4} onChange={(color4) => {setColor4(color4.hex)}}/>)}</span></p>
+              <p>{color5}<button onClick={() => setShowColor5(showColor5 => !showColor5)} className='pick'  style={{backgroundColor: color5}}/><span>{showColor5 && (<ChromePicker color={color5} onChange={(color5) => {setColor5(color5.hex)}}/>)}</span></p>
+
           </div>
           <div className='btnBox'>
               <button onClick={cancelBtn} className='btn-p btnCancel'>Cancel</button>
